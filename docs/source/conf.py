878677609b9bd79c01a6a41a391ea60da458b74f
Amraa <amarsanaaganbaatar0409@gmail.com>
2026-02-06 18:37:44 +0800

    docs (#33)

---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "ebarimt-pos-sdk"
+copyright = "2026, Amraa1"
+author = "Amraa1"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+language = "mn"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
---
+++ docs/source/index.md
@@ -0,0 +1,11 @@
+# ebarimt-pos-sdk
+
+Modern async-first Python SDK for **Ebarimt POS API 3.0**.
+
+```{toctree}
+:maxdepth: 2
+
+installation
+quickstart
+api
+```

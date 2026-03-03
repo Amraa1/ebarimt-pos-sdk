0970b12b533066e67363ff8f36ab7a78409fa1a7
Amraa1 <amarsanaaganbaatar0409@gmail.com>
2026-03-03 15:44:07 +0800

    fixing test cases

--- tests/mock/test_tin_info.py
+++ tests/mock/test_tin_info.py
@@ -3,7 +3,7 @@ from ..data.auth import SUCCESS_RESPONSE as AUTH_SUCCESS_RESPONSE
 from ..helpers import TOKEN_URL, CLIENT_ID, PASSWORD, USERNAME, BASE_API_URL
 import respx
 import httpx
-from ebarimt_pos_sdk import api_settings_for_env, Environment, EbarimtApiClient
+from ebarimt_pos_sdk import EbarimtApiClient, ApiClientSettings
 
 
 @respx.mock
@@ -17,12 +17,13 @@ def test_tin_info_sync_ok() -> None:
         json=AUTH_SUCCESS_RESPONSE,
     ))
     
-    settings = api_settings_for_env(
-        Environment.STAGING, 
-        client_id=CLIENT_ID, 
-        username=USERNAME, 
-        password=PASSWORD,
-        )
+    settings = ApiClientSettings(
+        base_url=BASE_API_URL,
+        token_url=TOKEN_URL,
+        client_id=CLIENT_ID,
+        username=USERNAME,
+        password=PASSWORD
+    )
     with EbarimtApiClient(settings=settings) as client:
         resp = client.tin_info.read("5051045")
         assert resp.data == "16000859970"
